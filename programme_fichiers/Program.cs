52ee55079e22a906ac9851a307963f73cc994ce9
AseglioChristophe <asegliochristophe@gmail.com>
2022-11-09 04:34:14 +0100

    progamme_fichiers + programme_json

--- programme_fichiers/Program.cs
+++ programme_fichiers/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 namespace progamme_fichiers
 {
@@ -17,8 +18,12 @@ namespace progamme_fichiers
             }
 
             string filename = "monFichier.txt";
+            string filename2 = "monFichier.txt";
+
 
             string pathAndFile = Path.Combine(path, filename); // pour que ce soit crossplatform \\windows //linux
+            string pathAndFile2 = Path.Combine(path, filename2); 
+
 
             if (File.Exists(pathAndFile))
             {
@@ -39,23 +44,78 @@ namespace progamme_fichiers
                 "Martin"
             };*/
 
-            string texte = "";
-            int nbLignes = 20000;
+            // immutable
+            //texte = "toto";
+            //texte += "o"; //totoo
 
+            //mutable
+            //StringBuilder
+
+            StringBuilder texte = new StringBuilder();
+            //string texte = "";
+            int nbLignes = 10000000;
+
+            /***************************************************************************************************/
+            /*DateTime t1 = DateTime.Now;
+            Console.WriteLine("Préparation des données... ");
             for(int i = 1; i < nbLignes; i++)
             {
-                texte = "Ligne " + i;
+                texte.Append("Ligne " + i + "\n");
             }
 
-            Console.Write();
-            File.WriteAllLines(pathAndFile, texte); //AllLines et AllText est le même
+            Console.WriteLine("OK");
+
+            Console.WriteLine("Ecriture des données ...");
+            File.WriteAllText(pathAndFile, texte.ToString()); //AllLines et AllText est le même sauf si on utilise StringBuilder 
+            Console.WriteLine("OK");
+
+            DateTime t2 = DateTime.Now;
+            var diff = (int)(t2 - t1).TotalMilliseconds;   //1s = 1000ms
+            Console.WriteLine("Durée (ms)" + diff);*/
+            /*******************************************************************************************************/
+
+            //Stream : flux //Pour écrire des très gros fichers !
+            //using (variable...) {...code ... } pour alouer la mémoire et la libérer à la fin de l'instruction
+
+            DateTime t1 = DateTime.Now;
+            Console.WriteLine("Ecriture des données... ");
+            using (var writeStream = File.CreateText(pathAndFile)) 
+            {
+                for (int i = 1; i < nbLignes; i++)
+                {
+                    writeStream.Write("Ligne " + i + "\n");
+                }
+            }
+            Console.WriteLine("OK");
+            DateTime t2 = DateTime.Now;
+            var diff = (int)(t2 - t1).TotalMilliseconds;   //1s = 1000ms
+            Console.WriteLine("Durée (ms)" + diff);
+
+            //Lecture avec un stream
+            using (var readStream = File.OpenText(pathAndFile))
+            {
+                while(true)
+                {
+                    var line = readStream.ReadLine();
+                    if(line == null)
+                    {
+                        break;
+                    }
+                    Console.WriteLine(line);
+                }
+            }
+
+
+
+
+
             //File.WriteAllText("monFichier.txt", "Voici le contenu que j'écris dans mon fichier texte"); //crée et écrase le fichier
             //File.AppendAllText(filename, "je rajoute ce texte.");
 
             try
             {
                 //string resultat = File.ReadAllText(filename);
-                var lignes = File.ReadAllLines(pathAndFile);
+                var lignes = File.ReadAllLines(pathAndFile); //il met en mémoire le fichier en une fois (out of memory)!
 
                 foreach (var ligne in lignes)
                 {
@@ -71,6 +131,9 @@ namespace progamme_fichiers
             {
                 Console.WriteLine("Une erreur inconnue est arrivée");
             }
+            //File.Copy(pathAndFile, pathAndFile2);
+            //File.Delete(pathAndFile2);
+            File.Move(pathAndFile, pathAndFile2); //sert à déplacer ou à renomer 
         }
     }
 }
---
+++ programme_json/Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace programme_json
+{
+    class Personne
+    {
+        public string nom { get; private set; }
+        public int age { get; private set; }
+        public bool majeur { get; private set; }
+
+        public Personne(string nom, int age, bool majeur)
+        {
+            this.nom = nom;
+            this.age = age;
+            this.majeur = majeur;
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine("Nom: " + nom + " - age: " + age + " ans");
+        }
+
+    }
+
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            var personne1 = new Personne("Toto", 20, true); //{  nom = "Toto", age = 20, majeur = true }; sans passer par constructeur
+            personne1.Afficher();
+
+            string json = JsonConvert.SerializeObject(personne1);
+            Console.WriteLine(json);
+
+            string jsonTiti = "{\"nom\":\"Titi\",\"age\":15,\"majeur\":false}"; // ajouter un backslash à chaque guillemet
+            Personne titi = JsonConvert.DeserializeObject<Personne>(jsonTiti); //operation inverse "deserialize"
+            titi.Afficher();
+        }
+    }
+}
+
+//click droit sur l'onglet dépendance => gérer les packages nuget. Puis installer Newtonsoft Json
+//Json editor online pour interpreter du format json
+//Le Json doir pouvoir accéder aux données soit à travers le constructeur soit par les variables ou les propriétés (get; set;) sinon il ne deserializera pas
+//Json ne transporte que des données, pas des fonctions 
